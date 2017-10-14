cbd2bdee9d72f85224fb8d94865251d8c44c119e
Elise Kain <elise.kain@gmail.com>
2017-10-14 18:08:25 -0400

    add facilities images carousel

--- assets/all.js
+++ assets/all.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
 	// Enable left menu for mobile
 	$(".button-collapse").sideNav();
 
+	// Enable carousel of images for facilities page
+	$(".carousel").carousel();
+
 	// Set copyright year
 	var year = new Date();
 	year = year.getFullYear();
