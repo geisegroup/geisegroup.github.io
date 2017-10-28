0b5bfdd7951461d6680f26b629e78ce405a09242
Elise Kain <elise.kain@gmail.com>
2017-10-28 12:29:39 -0400

    rotate facilities accordion dropdown arrows

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,58 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const minify = require('gulp-minify-css');
-const sass = require('gulp-sass');
-const child = require('child_process');
-const gutil = require('gulp-util');
-const browserSync = require('browser-sync').create()
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const cleanCSS = require("gulp-clean-css");
+const sass = require("gulp-sass");
+const child = require("child_process");
+const gutil = require("gulp-util");
+const browserSync = require("browser-sync").create();
 
-const cssFiles = '_css/**/*.?(s)css';
-const jsFiles = '_js/*.js';
-const siteRoot = '_site';
+const cssFiles = "_css/**/*.?(s)css";
+const jsFiles = "_js/*.js";
+const siteRoot = "_site";
 
-gulp.task('css', () => {
-	gulp.src(cssFiles)
-	.pipe(sass())
-	.pipe(concat('all.css'))
-	.pipe(minify())
-	.pipe(gulp.dest('assets'))
+gulp.task("css", () => {
+	gulp
+		.src(cssFiles)
+		.pipe(sass())
+		.pipe(concat("all.css"))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest("assets"));
 });
 
-gulp.task('js', () => {
-	gulp.src(jsFiles)
-	.pipe(concat('all.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('assets'))
+gulp.task("js", () => {
+	gulp
+		.src(jsFiles)
+		.pipe(concat("all.js"))
+		.pipe(uglify())
+		.pipe(gulp.dest("assets"));
 });
 
-gulp.task('jekyll', () => {
-	const jekyll = child.spawn('jekyll', ['build',
-	'--watch',
-	'--incremental',
-	'--drafts'
-]);
+gulp.task("jekyll", () => {
+	const jekyll = child.spawn("jekyll", ["build", "--watch", "--incremental", "--drafts"]);
 
-const jekyllLogger = (buffer) => {
-	buffer.toString()
-	.split(/\n/)
-	.forEach((message) => gutil.log('Jekyll: ' + message));
-};
+	const jekyllLogger = buffer => {
+		buffer
+			.toString()
+			.split(/\n/)
+			.forEach(message => gutil.log("Jekyll: " + message));
+	};
 
-jekyll.stdout.on('data', jekyllLogger);
-jekyll.stderr.on('data', jekyllLogger);
+	jekyll.stdout.on("data", jekyllLogger);
+	jekyll.stderr.on("data", jekyllLogger);
 });
 
-gulp.task('serve', () => {
+gulp.task("serve", () => {
 	browserSync.init({
-		files: [siteRoot + '/**'],
+		files: [siteRoot + "/**"],
 		port: 4000,
 		server: {
 			baseDir: siteRoot
 		}
 	});
-	
-	gulp.watch(cssFiles, ['css']);
-	gulp.watch(jsFiles, ['js']);
+
+	gulp.watch(cssFiles, ["css"]);
+	gulp.watch(jsFiles, ["js"]);
 });
 
-gulp.task('default', ['css', 'js', 'jekyll', 'serve']);
+gulp.task("default", ["css", "js", "jekyll", "serve"]);
